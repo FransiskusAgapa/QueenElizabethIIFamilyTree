c48964b5f2e5a8e25252c145b7a7ddf932b1c2ea
Fransiskus Agapa <agapaf@spu.edu>
2025-01-19 13:18:45 -0800

    added function asking chilcren

--- main.pl
+++ main.pl
@@ -1,22 +1,22 @@
 % To Run: swipl > [main].
 
 % Define the fact
-status(frans, billionaire).
+status("Frans", "Billionaire").
 
 % Rules of Queen Elizabeth II
 
 % parents
-queen_father(george, father).
-queen_mother(elizabeth, mother).
+queen_father("George", "Father").
+queen_mother("Elizabeth", "Mother").
 
 % sibling
-queen_sister(margaret, sister).
+queen_sister("Margaret", "Sister").
 
-% grandchildren
-queen_1st_son(charles, 1st_son).
-queen_daughter(anne, daughter)
-queen_2nd_son(andrew, 2nd_son).
-queen_3rd_son(edward, 3rd_son).
+% children
+queen_1st_son("Charles", "1st son").
+queen_daughter("Anne", "Daughter").
+queen_2nd_son("Andrew", "2nd son").
+queen_3rd_son("Edward", "3rd son").
 
 show_queen_mother :-
     write("> "),
@@ -45,3 +45,27 @@ who_is_the_father :-
 
 who_is_the_mother :-
     show_queen_mother.
+
+who_is_1st_son :-
+    queen_1st_son(Name,Title),
+    write(Name),
+    write(" is the "),
+    write(Title),nl. 
+
+who_is_2nd_son :-
+    queen_2nd_son(Name, Title),
+    write(Name),
+    write(" is the "),
+    write(Title).
+
+who_is_3rd_son :-
+    queen_3rd_son(Name,Title),
+    write(Name),
+    write(" is the "),
+    write(Title).
+
+who_is_the_daughter :-
+    queen_daughter(Name,Title),
+    write(Name),
+    write(" is the "),
+    write(Title).
